331d9dc18b619158df6082d9cc41e0e473c6359f
whyudacok <119993190+whyudacok@users.noreply.github.com>
2024-08-23 20:43:39 +0700

    Create Navbar.svelte

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,22 @@
+<nav>
+  <a href="/">Home</a>
+  <a href="/anime">Anime</a>
+  <!-- Tambahkan link lainnya sesuai kebutuhan -->
+</nav>
+
+<style>
+  nav {
+    display: flex;
+    justify-content: space-around;
+    padding: 1rem;
+    background: #333;
+    color: white;
+  }
+  a {
+    color: white;
+    text-decoration: none;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+</style>
