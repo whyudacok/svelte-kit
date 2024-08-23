cf473be815396fb6e4beef5dd65247f4f94eab59
whyudacok <119993190+whyudacok@users.noreply.github.com>
2024-08-23 23:01:45 +0700

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
   import Navbar from '../components/Navbar.svelte'; // jalur relatif dari src/routes ke src/components
   import Footer from '../components/Footer.svelte'; // jalur relatif dari src/routes ke src/components
   import '../app.css'; // jalur relatif dari src/routes ke src/app.css
+  import '../assets/cihuy.css';
+  import '../assets/cihuy2.css';
+  import '../assets/cihuy3.css';
+  import '../assets/cihuy4.css';
 </script>
 
 <Navbar />
