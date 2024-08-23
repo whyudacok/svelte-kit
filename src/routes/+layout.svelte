7c6bf2808f3968dc051fc423cfd71481ff0907d4
whyudacok <119993190+whyudacok@users.noreply.github.com>
2024-08-23 20:46:55 +0700

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,9 @@
 <script>
-  import Navbar from './components/Navbar.svelte';
-  import Footer from './components/Footer.svelte';
-  import "../app.css";
+  import Navbar from '$lib/components/Navbar.svelte'; // atau ../components/Navbar.svelte jika tidak menggunakan alias
+  import Footer from '$lib/components/Footer.svelte'; // atau ../components/Footer.svelte
+  import '../app.css'; // pastikan jalur ini benar
 </script>
+
 
 <Navbar />
 <main>
