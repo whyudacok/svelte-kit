79caad8409ca9a0af3224f9c10cf663e1c41c708
whyudacok <119993190+whyudacok@users.noreply.github.com>
2024-08-23 23:47:00 +0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
-
 <script>
-  import komikcuy from '../components/KomikUpdate.svelte';
+  import KomikUpdate from '../components/KomikUpdate.svelte';
   import AnimeUpdate from '../components/AnimeUpdate.svelte';
 </script>
 
@@ -228,9 +227,8 @@
             </div>
             <div></div>
             <div>
-                <AnimeUpdate />
-
-                <komikcuy />
+           <AnimeUpdate />
+          <KomikUpdate />
               <div class="scroll-smooth flex flex-col mx-auto my-1 md:my-0">
                 <div class="flex items-center gap-2 px-3 xl:px-0">
                   <span class="w-[0.35rem] h-6 md:w-[0.3rem] rounded-md bg-white"></span>
